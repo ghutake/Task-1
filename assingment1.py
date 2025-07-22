66d668d5647a2373ac43d882b72590f838c6bc54
ghutake <aniketghutake888@gmail.com>
2025-07-22 13:47:52 +0530

    Add files via upload

---
+++ assingment1.py
@@ -0,0 +1,10 @@
+a=float(input('enter first number: '))
+b=float(input('enter second nmuber: '))
+addition=a+b
+print('addition: ',addition)
+subtraction=a-b
+print('subtraction: ',subtraction)
+multiplication=a*b
+print('multiplication: ',multiplication)
+division=a/b
+print('division: ',division)
